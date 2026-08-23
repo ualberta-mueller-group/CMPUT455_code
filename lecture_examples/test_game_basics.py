bb09fbb5125a51ffc774019cf96d7256f37351dc
Martin Müller <mmueller@ualberta.ca>
2026-08-23 14:31:49 -0600

    move from old repo; change encoding EMPTY = 2

---
+++ lecture_examples/test_game_basics.py
@@ -0,0 +1,31 @@
+# Cmput 455 unit tests for sample code
+# game_basics.py: Game basics - constants and definitions for two player games
+# Written by Martin Mueller
+
+from game_basics import EMPTY, BLACK, WHITE, BORDER, is_black_white, \
+is_empty_black_white, opponent, color_as_string, winner_as_string
+
+assert(BLACK == 0)
+assert(WHITE == 1)
+assert(EMPTY == 2)
+assert(BORDER == 3)
+
+assert(is_black_white(BLACK))
+assert(is_black_white(WHITE))
+assert(not is_black_white(EMPTY))
+
+assert(is_empty_black_white(BLACK))
+assert(is_empty_black_white(WHITE))
+assert(is_empty_black_white(EMPTY))
+assert(not is_empty_black_white(4))
+assert(not is_empty_black_white(-1))
+
+assert(opponent(BLACK) == WHITE)
+assert(opponent(WHITE) == BLACK)
+
+assert(color_as_string(BLACK) == "Black")
+assert(color_as_string(WHITE) == "White")
+
+assert(winner_as_string(BLACK) == "Black")
+assert(winner_as_string(WHITE) == "White")
+assert(winner_as_string(EMPTY) == "Draw")
