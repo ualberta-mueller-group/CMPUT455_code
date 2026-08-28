da18737b29ea5ec146b3aae8b7e8a8a2543a3dd4
Martin Müller <mmueller@ualberta.ca>
2026-08-28 17:41:38 -0600

    move from old repo and update

--- lecture_examples/test_game_basics.py
+++ lecture_examples/test_game_basics.py
@@ -5,27 +5,31 @@
 from game_basics import EMPTY, BLACK, WHITE, BORDER, is_black_white, \
 is_empty_black_white, opponent, color_as_string, winner_as_string
 
-assert(BLACK == 0)
-assert(WHITE == 1)
-assert(EMPTY == 2)
-assert(BORDER == 3)
+def test_game_basics() -> None:
+    assert(BLACK == 0)
+    assert(WHITE == 1)
+    assert(EMPTY == 2)
+    assert(BORDER == 3)
 
-assert(is_black_white(BLACK))
-assert(is_black_white(WHITE))
-assert(not is_black_white(EMPTY))
+    assert(is_black_white(BLACK))
+    assert(is_black_white(WHITE))
+    assert(not is_black_white(EMPTY))
 
-assert(is_empty_black_white(BLACK))
-assert(is_empty_black_white(WHITE))
-assert(is_empty_black_white(EMPTY))
-assert(not is_empty_black_white(4))
-assert(not is_empty_black_white(-1))
+    assert(is_empty_black_white(BLACK))
+    assert(is_empty_black_white(WHITE))
+    assert(is_empty_black_white(EMPTY))
+    assert(not is_empty_black_white(4))
+    assert(not is_empty_black_white(-1))
 
-assert(opponent(BLACK) == WHITE)
-assert(opponent(WHITE) == BLACK)
+    assert(opponent(BLACK) == WHITE)
+    assert(opponent(WHITE) == BLACK)
 
-assert(color_as_string(BLACK) == "Black")
-assert(color_as_string(WHITE) == "White")
+    assert(color_as_string(BLACK) == "Black")
+    assert(color_as_string(WHITE) == "White")
 
-assert(winner_as_string(BLACK) == "Black")
-assert(winner_as_string(WHITE) == "White")
-assert(winner_as_string(EMPTY) == "Draw")
+    assert(winner_as_string(BLACK) == "Black")
+    assert(winner_as_string(WHITE) == "White")
+    assert(winner_as_string(EMPTY) == "Draw")
+
+if __name__ == "__main__":
+    test_game_basics()
